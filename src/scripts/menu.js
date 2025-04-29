0fcf224dc6ae5aef90346a96bedd9bcef8319d80
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-04-29 17:48:18 -0300

    teste 15

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("primeiro");
         
     } else if(pathParts[1] === "ESTEC"){
-        basePath = "ESTEC/"
+        basePath = ""
         console.log("segundo");
 
     } else {
