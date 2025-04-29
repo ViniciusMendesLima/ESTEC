e173aa515d7859d48d113ea95166cda9785154b4
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-04-29 14:48:27 -0300

    teste 6

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -5,8 +5,17 @@ import { setGreeting } from "./greeting.js";
 export function setMenu(){
 document.addEventListener("DOMContentLoaded", () => {
     const pathParts = window.location.pathname.split("/");
-    const basePath = pathParts[1] ? `/${pathParts[1]}` : "";
+    console.log(pathParts);
+    
+    let basePath
+    if (pathParts[1] === "index.html") {
+        basePath = ""
+    } else {
+        basePath = "../.."
+    }
+    console.log(basePath);
     const fullPath = `${basePath}/src/pages/menu.html`;
+    console.log(fullPath);
 
     fetch(fullPath)
     .then(response => response.text())
