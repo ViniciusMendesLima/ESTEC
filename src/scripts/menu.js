4c5cff9aa4fd19679ef8c167d5e4ed263d9fbc81
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-04-29 14:26:59 -0300

    teste 4

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,10 +1,13 @@
 import { setGreeting } from "./greeting.js";
 
-const autlaPath = window.location.pathname.includes("pages") ? "../" : "src/";
+//const autlaPath = window.location.pathname.includes("pages") ? "../" : "src/";
 
 export function setMenu(){
 document.addEventListener("DOMContentLoaded", () => {
-    fetch(`${autlaPath}pages/menu.html`)
+    const basePath = window.location.pathname.split("/")[1];
+    const fullPath = `/${basePath}/src/pages/menu.html`;
+
+    fetch(fullPath)
     .then(response => response.text())
     .then(data => {
         document.getElementById("menu").innerHTML = data;
