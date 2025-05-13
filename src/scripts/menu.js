3527ace239e2cb61317f90f5261dbbd653b8c9c3
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-05-13 20:24:17 -0300

    teste git page

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,7 +3,7 @@ import { setGreeting } from "./greeting.js";
 export function setMenu(){
 document.addEventListener("DOMContentLoaded", () => {
     const pathParts = window.location.pathname.split("/");
-    // console.log(pathParts);
+     console.log(pathParts);
     let basePath
     if (pathParts[1] === "index.html") {
         basePath = " "
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     // console.log(basePath);
     const fullPath = `${basePath}src/pages/menu.html`;
-    // console.log("caminho completo: "+fullPath);
+     console.log("caminho completo: "+fullPath);
 
     fetch(fullPath)
     .then(response => response.text())
