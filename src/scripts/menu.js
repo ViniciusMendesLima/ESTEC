01d3a77f4ce1ec8026d786f060e579f32e297d39
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-05-13 20:48:44 -0300

    teste git page

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,6 +3,7 @@ import { setGreeting } from "./greeting.js";
 export function setMenu() {
   document.addEventListener("DOMContentLoaded", () => {
     const isLocal = location.hostname === "127.0.0.1" || location.hostname === "localhost";
+    console.log(isLocal);
     
     // Caminho base conforme ambiente
     const basePath = isLocal ? "/src/pages/menu.html" : "https://viniciusmendeslima.github.io/ESTEC/src/pages/menu.html";
