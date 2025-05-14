e69e0a5c2ed15c867871d5ac2bd0536b50a84370
Vinícius M Lima <viniciusmendeslima@yahoo.com>
2025-05-13 21:29:23 -0300

    teste git page

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,63 +1,38 @@
+
+
+
 import { setGreeting } from "./greeting.js";
 
-export function setMenu() {
-  document.addEventListener("DOMContentLoaded", () => {
-    const isLocal = location.hostname === "127.0.0.1" || location.hostname === "localhost";
-    const repoName = "/ESTEC";
+export function setMenu(){
+document.addEventListener("DOMContentLoaded", () => {
+    const pathParts = window.location.pathname.split("/");
+     console.log(pathParts);
+    let basePath
+    if (pathParts[1] === "index.html") {
+        basePath = " "
+         console.log("primeiro");
+        
+    } else if(pathParts[1] === "ESTEC"){
+        basePath = ""
+         console.log("segundo");
 
-    const basePath = isLocal
-      ? "/src/pages/menu.html"
-      : `${repoName}/src/pages/menu.html`;
+    } else {
+        basePath = "https://viniciusmendeslima.github.io/ESTEC/"
+         console.log("terceiro");
 
-    fetch(basePath)
-      .then(response => {
-        if (!response.ok) throw new Error("Erro ao buscar menu: " + response.status);
-        return response.text();
-      })
-      .then(data => {
+    }
+    // console.log(basePath);
+    const fullPath = `${basePath}src/pages/menu.html`;
+     console.log("caminho completo: "+fullPath);
+
+    fetch(fullPath)
+    .then(response => response.text())
+    .then(data => {
         document.getElementById("menu").innerHTML = data;
         setGreeting();
-      })
-      .catch(error => {
+    })
+    .catch(error => {
         console.error("Erro ao carregar o menu:", error);
-      });
+    });
   });
 }
-
-
-
-// import { setGreeting } from "./greeting.js";
-
-// export function setMenu(){
-// document.addEventListener("DOMContentLoaded", () => {
-//     const pathParts = window.location.pathname.split("/");
-//      console.log(pathParts);
-//     let basePath
-//     if (pathParts[1] === "index.html") {
-//         basePath = " "
-//          console.log("primeiro");
-        
-//     } else if(pathParts[1] === "ESTEC"){
-//         basePath = ""
-//          console.log("segundo");
-
-//     } else {
-//         basePath = "https://viniciusmendeslima.github.io/ESTEC/"
-//          console.log("terceiro");
-
-//     }
-//     // console.log(basePath);
-//     const fullPath = `${basePath}src/pages/menu.html`;
-//      console.log("caminho completo: "+fullPath);
-
-//     fetch(fullPath)
-//     .then(response => response.text())
-//     .then(data => {
-//         document.getElementById("menu").innerHTML = data;
-//         setGreeting();
-//     })
-//     .catch(error => {
-//         console.error("Erro ao carregar o menu:", error);
-//     });
-//   });
-// }
